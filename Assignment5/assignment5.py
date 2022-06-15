c65921b62d928c2e5289a425dac863f988219dd9
apirzadeh1365 <a.pirzadeh@st.hanze.nl>
2022-06-15 15:08:07 +0200

    change the name of csv

--- Assignment5/assignment5.py
+++ Assignment5/assignment5.py
@@ -78,4 +78,4 @@ Az_data=pd.DataFrame(columns=['Question_number','Answer','Explain'])
 Az_data.Question_number=np.arange(1,11)
 Az_data.Answer=[A1,A2,A3,A4,A5,A6,A7,A8,A9,A10]
 Az_data.Explain=[E1,E2,E3,E4,E5,E6,E7,E8,E9,E10]
-Az_data.to_csv('output/Az.csv', index=False,header=True)
+Az_data.to_csv('output/assignment5.csv', index=False,header=True)
