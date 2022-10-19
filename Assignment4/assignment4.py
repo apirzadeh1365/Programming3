83cca1063eae7c42d59e74dfc3364f0c29c80db9
apirzadeh1365 <a.pirzadeh@st.hanze.nl>
2022-10-19 15:57:00 +0200

    update

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -2,19 +2,19 @@ from Bio import SeqIO
 import sys
 
 dir = sys.argv[1]
-kmer_list = []
+list_of_kemer = []
 with open("output/"+dir+"/contigs.fa") as handle:
     for record in SeqIO.parse(handle, "fasta"):
-        sequence = record.seq
-        kmer_list.append([len(sequence)])
-kmer_list.sort(reverse=True, key=lambda x: x[0])
-num_list = [item[0] for item in kmer_list]
-average = (sum(num_list))/2
-sum_num = 0
+        seque = record.seq
+        list_of_kemer.append([len(seque)])
+list_of_kemer.sort(reverse=True, key=lambda x: x[0])
+list_of_number = [item[0] for item in list_of_kemer]
+avg = (sum(list_of_number))/2
+sum_of_number = 0
 
-for kmer in kmer_list:
-    sum_num = sum_num + kmer[0]
-    if sum_num >= average:
-        n50 = kmer[0]
+for kmr in list_of_kemer:
+    sum_of_number = sum_of_number + kmr[0]
+    if sum_of_number >= avg:
+        n50 = kmr[0]
         break
 print(dir,n50)
