583efdc56ba0781f1d79179cc384d899f91faa68
apirzadeh1365 <a.pirzadeh@st.hanze.nl>
2022-10-24 20:07:04 +0200

    update

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -4,21 +4,21 @@ Azadeh Pirzadeh
 """
 from Bio import SeqIO
 import sys
+import numpy as np
 
 dir = sys.argv[1]
 list_of_kemer = []
 with open("output/"+dir+"/contigs.fa") as handle:
     for record in SeqIO.parse(handle, "fasta"):
         sequence = record.seq
-        list_of_kemer.append([len(sequence)])
-list_of_kemer.sort(reverse=True, key=lambda x:x[0])
-list_of_number = [item[0] for item in list_of_kemer]
-ave = (sum(list_of_number))/2
+        list_of_kemer.append(len(sequence))   
+list_of_kemer.sort(reverse=True)
+threshold = (np.sum(list_of_kemer))/2
 
 sum_number = 0
-for kmer in list_of_kemer:
-    sum_number = sum_number + kmer[0]
-    if sum_number >= ave:
-        n50 = kmer[0]
+for kmr in list_of_kemer:
+    while sum_number <= threshold:
+        sum_number = sum_number + kmr
+        n50 = kmr
         break
 print(dir,n50)
--- Assignment4/assignment5.py
+++ Assignment4/assignment5.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import shutil as sh
 
+
 df=pd.read_csv('output/kmer_result.csv',sep=' ',names=['kmr','n50'])
 i=np.max(df['n50'])
 number_i=df[df['n50']==i]
